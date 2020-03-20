a8e1ab10cd7dbfce6c19edf3c0df422f6d853091
unknown <agaevramesh@gmail.com>
2020-03-20 23:05:22 +0300

    task 4

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const helloName = () => {
   console.log('Welcome to the Brain Games!\n');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-}
+};
 
 export default helloName;
