fdc996c6bf99eb2aee24ab502b47301471e0e674
unknown <agaevramesh@gmail.com>
2020-03-30 20:52:07 +0300

    task 6

---
+++ bin/brain-calc.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { helloName, fCalcBrain } from '../src/index.js';
+
+fCalcBrain(helloName());
--- src/index.js
+++ src/index.js
@@ -7,20 +7,45 @@ const helloName = () => {
   return userName;
 };
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
 const fEvenBrain = (userName) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i <= 2; i += 1) {
-    const randomNum = Math.round(Math.random() * 100);
+    const randomNum = getRandomInt(0, 100);
     const randomAnswer = readlineSync.question(`Question: ${randomNum}\nYour answer: `);
     if ((randomNum % 2 === 0 && randomAnswer === 'yes') || (randomNum % 2 === 1 && randomAnswer === 'no')) {
-      console.log('Correct!');
-    }
+      console.log('Correct!'); }
     else {
       console.log(`"${randomAnswer}" is wrong answer ;(. Correct answer was `, randomNum % 2 === 0 ? '"yes"' : '"no"');
       console.log(`Let's try again, ${userName}!`);
+      return 0;
     }
   }
   console.log(`Congratulations, ${userName}!`);
 };
 
-export { helloName, fEvenBrain };
+const fCalcBrain = (userName) => {
+  console.log('What is the result of the expression?');
+  for (let i = 0; i <= 2; i += 1) {
+    const randomNum1 = getRandomInt(0, 100);
+    const randomNum2 = getRandomInt(0, 100);
+    const randomNum3 = (getRandomInt(0, 3) === 0 ? '+' : 1 ? '-' : 2 ? '*' : null);
+    const randomAnswer = readlineSync.question(`Question: ${randomNum1} ${randomNum3} ${randomNum2}\nYour answer: `);
+    const tempAnswer = (randomNum3 === '+' ? randomNum1 + randomNum2 : '-' ? randomNum1 - randomNum2 : '*' ? randomNum1 * randomNum2 : null);
+    if (tempAnswer == randomAnswer) {
+      console.log('Correct!'); }
+    else {
+      console.log(`"${randomAnswer}" is wrong answer ;(. Correct answer was "${tempAnswer}"`);
+      console.log(`Let's try again, ${userName}!`);
+      return 0;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export { helloName, fEvenBrain, fCalcBrain };
