e48bffe69291b62e3553153c1c4c868195202a47
unknown <agaevramesh@gmail.com>
2020-03-30 20:55:37 +0300

    taks 6 light linter fix

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const helloName = () => {
 };
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  const min = Math.ceil(min);
+  const max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается 
 }
 
 const fEvenBrain = (userName) => {
