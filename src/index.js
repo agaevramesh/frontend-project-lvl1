5de2e9953f5cd2740fe1f4d13337dd59655d29d7
unknown <agaevramesh@gmail.com>
2020-04-09 17:57:21 +0300

    status

--- src/index.js
+++ src/index.js
@@ -27,8 +27,9 @@ const evenBrain = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   const randomNum = getRandomInt(0, 100);
   const randomAnswer = readlineSync.question(`Question: ${randomNum}\nYour answer: `);
-  if ((randomNum % 2 === 0 && randomAnswer === 'yes') || (randomNum % 2 === 1 && randomAnswer === 'no'))
+  if ((randomNum % 2 === 0 && randomAnswer === 'yes') || (randomNum % 2 === 1 && randomAnswer === 'no')) {
     return console.log('Correct!');
+  }
   else {
     console.log(`"${randomAnswer}" is wrong answer ;(. Correct answer was `, randomNum % 2 === 0 ? '"yes"' : '"no"');
     return 0;
